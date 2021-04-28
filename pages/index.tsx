ee91bd66d1527747080f2b300a72f705b25b201e
Randal Maia <randalmaia@gmail.com>
2021-04-28 02:26:47 -0300

    fix(header): shrink-to-fit

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,10 @@ const Home = ({ data }: HomeProps) => {
           rel="stylesheet"
         />
         <meta name="format-detection" content="telephone=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
       </Head>
       <Layout user={data.user}>
         <Dashboard
