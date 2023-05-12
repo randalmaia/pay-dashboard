2d6bd62d238d7983b9f0c9d4e02a7526b704c44c
Randal Maia <randalmaia@Randals-Laptop.local>
2023-05-12 11:16:35 -0300

    fix: hide keys

--- graphql/client.ts
+++ graphql/client.ts
@@ -4,9 +4,9 @@ const Client = new ApolloClient({
   uri: "https://parseapi.back4app.com/graphql",
   cache: new InMemoryCache(),
   headers: {
-    "X-Parse-Application-Id": "xGaPMFg4RajOwOZMbtfi8FNdZgSI4zXpTCfZka02",
-    "X-Parse-Master-Key": "ywKLuyMDVmDrXs159G9gjdV1sKFbLOxXeoZSaMcl",
-    "X-Parse-Client-Key": "jUemrHF3RoYV56IZ6WtWP4RNmBt32wFRZSXBDZH1",
+    "X-Parse-Application-Id": "...",
+    "X-Parse-Master-Key": "...",
+    "X-Parse-Client-Key": "...",
   },
 });
 
